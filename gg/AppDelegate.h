94e92dfbd6088b45b3b1a33f7179f5a5e9ba3d57
huxiaohong <1009681836@qq.com>
2020-05-15 09:52:43 +0800

    Feat:ff

---
+++ gg/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppDemo
+//
+//  Created by JiaMin Kuang on 2020/2/27.
+//  Copyright © 2020 JiaMin Kuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : FF_AJUtilsResponder <UIApplicationDelegate>
+
+
+@end
+
